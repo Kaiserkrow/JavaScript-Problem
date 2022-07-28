b7671d809a30480370d16ccab3b2143bccc1c0fe
Kaiserkrow <42108060+Kaiserkrow@users.noreply.github.com>
2022-07-29 00:15:22 +0800

    practicing spread operator and using destructuring method

---
+++ functions.js
@@ -0,0 +1,12 @@
+const multiplyByTwo = (x) => x * 2; //arrow function
+
+let num = 32;
+
+console.log(multiplyByTwo(num));
+
+const alphabet = ['A','B','C','D','E','F'];
+const numbers = ['1','2','3','4','5','6'];
+
+const [a,,b, ...d] = alphabet; //spread operator and using destructuring method
+
+console.log(a + b + " " + d); 
