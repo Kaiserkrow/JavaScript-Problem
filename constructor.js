18c110d2cfb1874d838176c8cfd376d2405cbe1c
Kaiserkrow <42108060+Kaiserkrow@users.noreply.github.com>
2022-07-29 15:10:36 +0800

    getter and setter added in constructor.js

--- constructor.js
+++ constructor.js
@@ -8,3 +8,24 @@ class Vegetable {
 
 const carrot = new Vegetable("carrot");
 console.log(carrot.name); // Should display 'carrot'
+
+
+//getter and setter
+
+class Book {
+  constructor(author) {
+    this._author = author;
+  }
+  // getter
+  get writer() {
+    return this._author;
+  }
+  // setter
+  set writer(updatedAuthor) {
+    this._author = updatedAuthor;
+  }
+}
+const novel = new Book("anonymous");
+console.log(novel.writer);
+novel.writer = "newAuthor";
+console.log(novel.writer);
