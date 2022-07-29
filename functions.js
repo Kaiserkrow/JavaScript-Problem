b7d9d453ea9fcd32173b542a02608bb671ae86ef
Kaiserkrow <42108060+Kaiserkrow@users.noreply.github.com>
2022-07-30 00:24:21 +0800

    for major project 1

--- functions.js
+++ functions.js
@@ -10,5 +10,19 @@ const numbers = ['1','2','3','4','5','6'];
 const [a,,b, ...d] = alphabet; //spread operator and using destructuring method
 
 const newArray = [...alphabet, ...numbers];
-console.log(a + b + " " + d); 
+console.log(a + b + " " + d);
 console.log(newArray);
+
+//WHO IS THE SITE FOR? 
+
+//WHAT DO THEY EXPECT TO FIND OR DO THERE?
+
+// IS THIS WEBSITE'S PRIMARY AIM TO INFORM, TO SELL. OR TO AMUSE?
+
+// WHAT COMPETITOR SITES, IF ANY, EXIST, AND HOW SHOULD THIS SITE BE INSPIRED BY DIFFERENT THAN THOSE COMPETITORS
+
+//site map
+
+// based on research findings, designers generates ideas
+// designer creates an Information architecture
+// turn the ideas into sitemap, wireframes and prototypes
